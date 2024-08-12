3945d730e67e2f75a633257bd98bc209bdc1e07d
SonuMajhi68 <sonumajhi3368@gmail.com>
2024-08-13 00:47:21 +0530

    rewrote the code

--- extension.js
+++ extension.js
@@ -1,26 +1,123 @@
-/* extension.js
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
-import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 
-export default class PlainExampleExtension extends Extension {
+import St from 'gi://St';
+import GLib from 'gi://GLib';
+import Clutter from 'gi://Clutter';
+
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+
+export default class DayCounterExtension extends Extension {
+    _indicators = {};
+    _signalHandler = null;
+    _settings = null;
+
     enable() {
+        this._settings = this.getSettings();
+        let myArray = this._settings.get_strv('counter-list');
+
+        this._updateIndicators(myArray);
+
+        this._signalHandler = this._settings.connect("changed::counter-list", (setting,key) => {
+            const arr = setting.get_strv('counter-list');
+            this._removeIndicator();
+            this._updateIndicators(arr);
+        })
     }
 
     disable() {
+        if (this._settings !== null && this._signalHandler !== null) {
+            this._settings.disconnect(this._signalHandler);
+          }
+        for(let id in this._indicators) {
+            this._indicators[id].indicator.destroy();
+            GLib.source_remove(this._indicators[id].timeoutId);
+            // delete this._indicators[id];
+        };
+
+        this._signalHandler = null;
+        this._settings = null;
+        this._indicators = {};
+    }
+
+
+    // Update and Add Indicator to StatusBar
+    _updateIndicators(myArray) {
+
+        if(Object.keys(this._indicators).length !== 0){
+            for(let id in this._indicators) {
+                if (!myArray.includes(id)) {
+                    this._indicators[id].indicator.destroy();
+                    GLib.source_remove(this._indicators[id].timeoutId);
+                }
+            };
+        }
+        
+        
+
+        myArray.forEach(item => {
+            const val = item.split(',');
+
+            if (!this._indicators.hasOwnProperty(val[0])) {
+
+                this._indicators[val[0]] = {};
+                const indicator = new PanelMenu.Button(0.0, item, false);
+                let hbox = new St.BoxLayout({style_class: 'panel-status-menu-box' });
+                let icon = new St.Icon({icon_name: val[2], style_class: 'system-status-icon'});
+                let label = new St.Label({y_align: Clutter.ActorAlign.CENTER, text: this._getDays(val[4], val[5], val[6], val[3])});
+
+                hbox.add_child(icon);
+                hbox.add_child(label);
+                indicator.add_child(hbox);
+                
+                Main.panel.addToStatusArea(this.uuid + val[0], indicator);
+
+                const timeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 1000, () => {
+                    this._updateLabels(label, val[0], val[4], val[5], val[6], val[3]);
+                    return GLib.SOURCE_CONTINUE;
+                });
+
+                this._indicators[val[0]] = { indicator, label, timeoutId };
+
+            }
+        });
+    }
+
+
+    // Update the Lable at the Status bar
+    _updateLabels(label, ind, year, month, day, countType) {
+        
+        if (this._indicators.hasOwnProperty(ind))
+            label.set_text(this._getDays(year, month, day, countType))
+    }
+
+    _removeIndicator() {
+        for(let id in this._indicators) {
+            this._indicators[id].indicator.destroy();
+            GLib.source_remove(this._indicators[id].timeoutId);
+            // delete this._indicators[id];
+        };
+
+        this._indicators = {};
+    }
+
+
+    // Get the Date from user and return number of days from today
+    _getDays(year, month, day, countType) {
+        const targetDate = GLib.DateTime.new_local(year, month, day, 0, 0, 0); // Replace with your target year, month, day
+        const nowDate = GLib.DateTime.new_now_local();
+        let microsecond = 0;
+
+        if(countType == 'U'){
+            microsecond = nowDate.difference(targetDate);
+        }
+        else{
+            microsecond = targetDate.difference(nowDate);
+        }
+    
+        // const microsecond = Math.abs(targetDate.difference(nowDate));
+        const days = Math.floor(microsecond/864e8);
+
+        return `${days} Days`;
     }
 }
---
+++ prefs.js
@@ -0,0 +1,295 @@
+
+import Gio from 'gi://Gio';
+import Gtk from 'gi://Gtk';
+import Adw from 'gi://Adw';
+import GLib from "gi://GLib";
+import GObject from 'gi://GObject';
+
+import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+
+
+
+function formatTargetDate(year, month, day) {
+    const monthString = month < 10 ? `0${month}` : `${month}`;
+    const dayString = day < 10 ? `0${day}` : `${day}`;
+
+    return `${year}/${monthString}/${dayString}`;
+}
+
+
+function simpleHash() {
+    const timestamp = Date.now();       // Get current timestamp in milliseconds
+    const randomNum = Math.random();    // Add randomness for uniqueness
+
+    const hash = timestamp + randomNum; // Simple hash function (replace with a more robust one if needed)
+
+    return hash.toString().substring(7,13);
+}
+
+
+
+export default class DayCounterPreferences extends ExtensionPreferences {
+    fillPreferencesWindow(window) {
+        const settings = this.getSettings();
+        let groupList = {};
+
+
+        // Create a preferences page, with a single group
+        const page = new Adw.PreferencesPage({
+            title: _('General'),
+            icon_name: 'general-symbolic',
+        });
+
+        let addAppsButton = new Gtk.Button({
+            child: new Adw.ButtonContent({
+                icon_name: 'list-add-symbolic',
+                label: _('Add')
+            })
+        });
+
+        const group = new Adw.PreferencesGroup({
+            title: _('Counter'),
+            header_suffix: addAppsButton,
+            description: _('Create/Select your counter to add on tray'),
+        });
+
+        page.add(group);
+        window.add(page);
+
+
+        const refreshList = () => {
+            let list = settings.get_strv('counter-list');
+
+
+            for(let i in list){
+                let str = list[i].split(',');
+
+                if(!groupList.hasOwnProperty(str[0])){
+                    groupList[str[0]] = {};
+                    groupList[str[0]].ButtonBox = new Gtk.Box({
+                        orientation: Gtk.Orientation.HORIZONTAL,
+                        halign: Gtk.Align.CENTER,
+                        spacing: 5,
+                        hexpand: false,
+                        vexpand: false
+                    });
+                    groupList[str[0]].DeleteButton = new Gtk.Button({
+                        icon_name: 'edit-delete-symbolic',
+                        valign: Gtk.Align.CENTER,
+                        css_classes: ['error'],
+                        hexpand: false,
+                        vexpand: false
+                    });
+
+
+                    const appIcon = new Gtk.Image({
+                        icon_name: str[2],
+                        pixel_size: 16
+                    });
+
+                    groupList[str[0]].Row = new Adw.ActionRow({
+                        title: str[1],
+                        activatable: true
+                    });
+
+
+                    groupList[str[0]].ButtonBox.append(groupList[str[0]].DeleteButton);
+                    groupList[str[0]].Row.add_prefix(appIcon);
+                    groupList[str[0]].Row.add_suffix(groupList[str[0]].ButtonBox);
+                    
+                    group.add(groupList[str[0]].Row);
+                }
+            }
+            
+            
+
+            const copyGroupList = {...groupList};
+            for (let i in copyGroupList) {
+                // log(`this is test value: ${i}`);
+                
+                groupList[i].DeleteButton.connect('clicked', () => {
+                    // removeItems(list[i]);
+                    settings.set_strv('counter-list',
+                        settings.get_strv('counter-list').filter((id) => {  // this need to be fixed
+                            return id.split(',')[0] !== i;
+                        })
+                    );
+                    group.remove(groupList[i].Row);
+
+                    // log(`out : ${i}`);
+                    delete groupList[i];
+                    // for(let j in groupList)
+                    //     log(`in : ${j}`);
+
+                });
+            }
+        }
+
+
+
+        refreshList();
+
+        const addRow = () => {
+
+            const addDialog = new Adw.PreferencesDialog();
+
+            // Create preferences pages and groups here
+            const dialogPage = new Adw.PreferencesPage({
+                title: _('General'),
+                icon_name: 'preferences-general-symbolic'
+            });
+            addDialog.add(dialogPage);
+
+            const saveButton = new Gtk.Button({
+                child: new Adw.ButtonContent({
+                    icon_name: 'emblem-ok-symbolic',     // list-add-symbolic
+                    label: _('Save')
+                })
+            });
+
+            const dialogGroup = new Adw.PreferencesGroup({
+                title: _('Create Counter'),
+                header_suffix: saveButton
+            });
+            dialogPage.add(dialogGroup);
+
+
+            // Create a input feild for Counter Name
+            const counterName = new Adw.ActionRow({
+                title: _("Counter Name"),
+                subtitle: _("Hint: GATE Counter"),
+            });
+            dialogGroup.add(counterName);
+          
+            const counterNameEntryBuffer = new Gtk.EntryBuffer();
+            settings.bind("name-string", counterNameEntryBuffer, "text", Gio.SettingsBindFlags.DEFAULT);
+          
+            const counterNameEntry = new Gtk.Entry({
+                buffer: counterNameEntryBuffer,
+                marginTop: 10,
+                marginBottom: 10,
+                widthRequest: 168,
+                placeholderText: _("Counter")
+            });
+
+            counterName.add_suffix(counterNameEntry);
+            counterName.set_activatable_widget(counterNameEntry);
+
+
+
+            // Create a input feild for Counter Name
+            const counterIcon = new Adw.ActionRow({
+                title: _("Counter Icon"),
+                subtitle: _("Hint: network-vpn-symbolic"),
+            });
+            dialogGroup.add(counterIcon);
+          
+            const counterIconEntryBuffer = new Gtk.EntryBuffer();
+            settings.bind("icon-string", counterIconEntryBuffer, "text", Gio.SettingsBindFlags.DEFAULT);
+          
+            const counterIconEntry = new Gtk.Entry({
+                buffer: counterIconEntryBuffer,
+                marginTop: 10,
+                marginBottom: 10,
+                widthRequest: 168,
+                placeholderText: _("network-vpn-symbolic")
+            });
+
+            counterIcon.add_suffix(counterIconEntry);
+            counterIcon.set_activatable_widget(counterIconEntry);
+
+
+
+            const counterType = new Adw.SwitchRow({
+                title: _('Count Up/Down'),
+                subtitle: _('Toggle Count Up or Count Down'),
+            });
+            dialogGroup.add(counterType);
+            settings.bind("counter-boolean", counterType, 'active', Gio.SettingsBindFlags.DEFAULT);
+
+
+
+            // Create a input field for date
+            const targetYear = settings.get_int('target-year');
+            const targetMonth = settings.get_int('target-month');
+            const targetDay = settings.get_int('target-day');
+
+
+
+            const targetDate = new Adw.ActionRow({
+                title: _("Target Date"),
+                subtitle: _("Date in YYYY/MM/DD Format"),
+            });
+            dialogGroup.add(targetDate);
+            
+            const targetDateButton = new Gtk.MenuButton({
+                label: formatTargetDate(targetYear, targetMonth, targetDay),
+                marginTop: 10,
+                marginBottom: 10,
+            });
+            targetDate.add_suffix(targetDateButton);
+            targetDate.set_activatable_widget(targetDateButton);
+            
+            const targetDatePopOver = new Gtk.Popover();
+            targetDateButton.set_popover(targetDatePopOver);
+        
+            const targetCalender = new Gtk.Calendar();
+            targetCalender.select_day(GLib.DateTime.new_local(targetYear, targetMonth, targetDay, 0, 0, 0));
+            targetDatePopOver.set_child(targetCalender);
+
+            function syncSettings(calendar) {
+                const year = calendar.year;
+                const month = calendar.month + 1;
+                const day = calendar.day;
+          
+                targetDateButton.set_label(formatTargetDate(year, month, day));
+          
+                settings.set_int("target-year", year);
+                settings.set_int("target-month", month);
+                settings.set_int("target-day", day);
+            }
+
+            for (const event of ["day-selected", "prev-year", "next-year", "prev-month", "next-month"]) {
+                targetCalender.connect(event, (calendar) => {
+                    syncSettings(calendar);
+                });
+            }
+
+
+            saveButton.connect('clicked', () => prepList(addDialog));
+
+            addDialog.present(window);
+
+        };
+
+        const prepList = (addDialog) => {
+            const name = settings.get_string('name-string');
+            const icon = settings.get_string('icon-string');
+            const count = (settings.get_boolean('counter-boolean')) ? 'U' : 'D';
+            const year = settings.get_int('target-year');
+            const month = settings.get_int('target-month');
+            const day = settings.get_int('target-day');
+            const hash = simpleHash();
+                
+            const item = (`${hash},${name},${icon},${count},${year},${month},${day}`);
+
+            let list = settings.get_strv('counter-list');
+            settings.set_strv('counter-list', [...list, item]);
+
+            list = settings.get_strv('counter-list');
+            list.forEach((ele) => {
+                const val = ele.split(',')
+            })
+
+            
+            refreshList();
+
+            addDialog.close();
+        }
+
+
+        addAppsButton.connect('clicked', addRow);
+
+        window._settings = this.getSettings();
+    }
+}
